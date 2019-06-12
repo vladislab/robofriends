a3b4fdbf4289edb69cea07ed2216077bd2310189
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-11 22:49:15 -0400

    added work in progress files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{Component} from 'react';
+import CardList from './CardList';
+//import {robots} from './robots';
+import SearchBox from './SearchBox';
+import './App.css' ;
+import Scroll from './Scroll';
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+    onSearchChange =(event)=>{
+        this.setState({searchField: event.target.value});
+        
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(response=>{
+            return response.json();//convert feedback to json
+        }).then(users =>{
+            this.setState({robots:users});
+        });
+        
+    }
+    render(){
+        const filteredRobots = this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        if(this.state.robots.length===0){
+            return <h1>Loading...</h1>
+        }
+        else{
+            return(
+                <div className='tc'>
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots = {filteredRobots}/>
+                    </Scroll>
+                    
+                </div>
+                
+            );
+        }
+        
+    }
+
 }
 
 export default App;
